dae14077b98644982fd66ec3d466a6f3b73f0e2f
arvtechv <tnetcarlosdar@outlook.com>
2026-03-13 17:27:33 -0600

    feat: agregar columna category a productos

--- app/Product.php
+++ app/Product.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = [
-        'name', 'description', 'price', 'stock', 'image'
+           'name', 'description', 'price', 'stock', 'image', 'category'
     ];
 }
