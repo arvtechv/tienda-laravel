d61ba00538e49ee04a092e9f4ffa8346f757b87e
arvtechv <tnetcarlosdar@outlook.com>
2026-03-14 10:08:49 -0600

    feat: aplicar diseño strAPPberry

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+<div class="content-card">
+    <div class="content-header">Dashboard</div>
+    <div class="content-body">
+        @if (session('status'))
+            <div class="alert alert-success">{{ session('status') }}</div>
+        @endif
+        <p style="color:#686B75;">Bienvenido, {{ auth()->user()->name ?? 'Usuario' }}.</p>
     </div>
 </div>
 @endsection
